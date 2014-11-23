1a4a3fd0ed3e1625a9412da419dcacfbfb067065
rfkdali <rfkdali@gmail.com>
2014-11-24 00:08:07 +0100

    add validates

--- app/models/lien.rb
+++ app/models/lien.rb
@@ -1,2 +1,10 @@
 class Lien < ActiveRecord::Base
+	validates :title, presence: true,
+                    length: { minimum: 5 }
+    validates :description, presence: true,
+                    length: { minimum: 5 }
+    validates :author, presence: true,
+                    length: { minimum: 5 }
+    validates :lien, presence: true,
+                    length: { minimum: 5 }
 end
