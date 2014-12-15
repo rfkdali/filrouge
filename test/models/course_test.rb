0c59a4d2c1767a84b2f38c452edbe27d26d8232e
rfkdali <rfkdali@gmail.com>
2014-12-15 17:37:20 +0100

    add user model

--- test/models/course_test.rb
+++ test/models/course_test.rb
@@ -6,5 +6,15 @@ class CourseTest < ActiveSupport::TestCase
 	  courses = Course.new
 	  assert_not courses.save, "Saved the post without a title"
   end
-
 end
+
+  class GarrosTest < ActiveSupport::TestCase
+
+  	test "true" do
+  		assert_equal score(1), 1 
+  	end
+  end
+
+  def score(number)
+  	number
+  end
