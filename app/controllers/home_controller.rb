471cdf4f10f0b5090a52d3e0d2e8ec2c27a8c81c
rfkdali <rfkdali@gmail.com>
2015-01-26 18:42:25 +0100

    test

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
-	http_basic_authenticate_with name: ENV['filrouge_user'], password: ENV['filrouge_pass']
+	http_basic_authenticate_with name: ENV['APP_USER'], password: ENV['APP_PASS']
 	# authenticate_or_request_with_http_basic name: ENV["MON_LOGIN"], password: ENV["MON_PASS"]
 
 
