aa839f209da1627c0e8a7d0e8a5c0e7c39f33312
rfkdali <rfkdali@gmail.com>
2015-02-28 17:33:25 +0100

    fix js init

--- app/assets/javascripts/weather.js
+++ app/assets/javascripts/weather.js
@@ -1,29 +1,32 @@
 //= require jquery
 //= require jquery_ujs
-refresh()
-function refresh(){
-	$.ajax({
-		url: $('#weather-config').data('url')
-	}).done(function(data) {
-		document.getElementById('city').innerHTML = "LOADING... (to be fixed)";
-			paris = {
- 					"city": data.name,
-    			"temperature": parseInt(data.main.temp - 273),
-  				"skystate" : data.weather[0].description,
-    			"windSpeed": data.wind.speed,
-    			"check_refresh": data.dt
-    			};
+$( document ).ready(function() {
+	function refresh(){
+		$.ajax({
+			url: $('#weather-config').data('url')
+		}).done(function(data) {
+			document.getElementById('city').innerHTML = "LOADING... (to be fixed)";
+				paris = {
+	 					"city": data.name,
+	    			"temperature": parseInt(data.main.temp - 273),
+	  				"skystate" : data.weather[0].description,
+	    			"windSpeed": data.wind.speed,
+	    			"check_refresh": data.dt
+	    			};
 
-    	document.getElementById('city').innerHTML = paris.city;
-    	document.getElementById('temperature').innerHTML = "Temperature = " + paris.temperature + "°C";
-    	document.getElementById('wind').innerHTML = "Wind speed = " + paris.windSpeed + " m/s";
+	    	document.getElementById('city').innerHTML = paris.city;
+	    	document.getElementById('temperature').innerHTML = "Temperature = " + paris.temperature + "°C";
+	    	document.getElementById('wind').innerHTML = "Wind speed = " + paris.windSpeed + " m/s";
 
-    	if ( console && console.log ) {
-			console.log( "Sample of data:", data );
-			}
+	    	if ( console && console.log ) {
+				console.log( "Sample of data:", data );
+				}
 
-  });
-  	window.setTimeout(refresh, 5000);
-}
+	  });
+	  	window.setTimeout(refresh, 5000);
+	}
+	refresh()
+});
+
 
 
